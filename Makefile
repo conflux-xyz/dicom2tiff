67abfb6cb4d2aa029539f302c42e59170ddedd33
Alden Timme <alden.timme@gmail.com>
2025-12-10 11:38:40 -0800

    Initial

---
+++ Makefile
@@ -0,0 +1,5 @@
+clippy:
+	cargo clippy --all-targets --all-features -- -D warnings
+
+format:
+	cargo fmt
---
+++ src/main.rs
@@ -0,0 +1,308 @@
+use std::env;
+use std::fs;
+use std::io::{self, Read, Seek};
+use std::path::{Path, PathBuf};
+
+use dicom_core::value::Value as DicomValue;
+use dicom_dictionary_std::tags as dicom_tags;
+use tiff::encoder::TiffKind;
+use tiff::encoder::{TiffEncoder, TiffKindBig};
+use tiff::tags::{PhotometricInterpretation as TiffPhotometricInterpretation, Tag as TiffTag};
+
+type BoxErrorResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn is_dicom_file(path: &Path) -> bool {
+    fs::File::open(path)
+        .and_then(|mut f| {
+            f.seek(io::SeekFrom::Start(128))?;
+            let mut buf = [0u8; 4];
+            f.read_exact(&mut buf)?;
+            Ok(buf)
+        })
+        .map(|buf| buf == *b"DICM")
+        .unwrap_or(false)
+}
+
+fn get_dicom_files(path: &Path) -> BoxErrorResult<Vec<PathBuf>> {
+    let dir = if path.is_file() {
+        path.parent()
+            .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "No parent directory"))?
+    } else if path.is_dir() {
+        path
+    } else {
+        return Err("Path is not a file or directory".into());
+    };
+
+    let mut paths = Vec::new();
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() && is_dicom_file(&path) {
+            paths.push(path);
+        }
+    }
+
+    Ok(paths)
+}
+
+fn get_dicom_pyramid_files(paths: Vec<PathBuf>) -> BoxErrorResult<Vec<PathBuf>> {
+    let mut dcm_objects = Vec::new();
+    for path in paths {
+        let obj = dicom_object::OpenFileOptions::new()
+            .read_until(dicom_tags::PIXEL_DATA)
+            .open_file(&path)?;
+        let image_type = obj.element(dicom_tags::IMAGE_TYPE)?.to_multi_str()?;
+        // Only take pyramid levels
+        let vals: Vec<&str> = image_type.iter().map(|s| s.trim()).collect();
+        let v1 = ["ORIGINAL", "PRIMARY", "VOLUME", "NONE"];
+        let v2 = ["DERIVED", "PRIMARY", "VOLUME", "NONE"];
+        let v3 = ["DERIVED", "PRIMARY", "VOLUME", "RESAMPLED"];
+        if vals == v1 || vals == v2 || vals == v3 {
+            dcm_objects.push((path, obj));
+        }
+    }
+
+    // Sort descending by TOTAL_PIXEL_MATRIX_COLUMNS, so pyramid levels are in order from 0 up.
+    dcm_objects.sort_by(|a, b| {
+        let a_cols =
+            a.1.element(dicom_tags::TOTAL_PIXEL_MATRIX_COLUMNS)
+                .ok()
+                .and_then(|e| e.uint32().ok())
+                .unwrap_or(0);
+        let b_cols =
+            b.1.element(dicom_tags::TOTAL_PIXEL_MATRIX_COLUMNS)
+                .ok()
+                .and_then(|e| e.uint32().ok())
+                .unwrap_or(0);
+        b_cols.cmp(&a_cols)
+    });
+
+    let paths = dcm_objects
+        .iter()
+        .map(|(path, _)| path.clone())
+        .collect::<Vec<_>>();
+
+    Ok(paths)
+}
+
+fn dicom_photometric_interpretation_to_tiff(
+    dcm_photometric_interpretation: &str,
+) -> BoxErrorResult<(TiffPhotometricInterpretation, Option<[u16; 2]>)> {
+    match dcm_photometric_interpretation {
+        "MONOCHROME1" => Ok((TiffPhotometricInterpretation::BlackIsZero, None)),
+        "MONOCHROME2" => Ok((TiffPhotometricInterpretation::WhiteIsZero, None)),
+        "RGB" => Ok((TiffPhotometricInterpretation::RGB, None)),
+        "YBR_FULL" => Ok((TiffPhotometricInterpretation::YCbCr, Some([1, 1]))),
+        "YBR_FULL_422" => Ok((TiffPhotometricInterpretation::YCbCr, Some([2, 1]))),
+        "YBR_ICT" => Ok((TiffPhotometricInterpretation::YCbCr, None)),
+        _ => Err(format!(
+            "Unsupported photometric interpretation: {}",
+            dcm_photometric_interpretation
+        )
+        .into()),
+    }
+}
+
+fn main() -> BoxErrorResult<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: {} <directory or .dcm file> <.tiff file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let input_path = Path::new(&args[1]);
+    let output_path = Path::new(&args[2]);
+    let dicom_paths = get_dicom_files(input_path)?;
+    let dicom_pyramid_paths = get_dicom_pyramid_files(dicom_paths)?;
+
+    if dicom_pyramid_paths.is_empty() {
+        return Err("No pyramid levels found".into());
+    }
+
+    let output_file = fs::File::create(output_path)?;
+    let mut tiff = TiffEncoder::new_big(output_file)?;
+
+    for dcm_path in dicom_pyramid_paths {
+        let dcm_object = dicom_object::open_file(&dcm_path)?;
+        // TODO: Handle sparsely tiled DICOMs
+        let is_sparse = dcm_object
+            .element_opt(dicom_tags::DIMENSION_ORGANIZATION_TYPE)?
+            .and_then(|e| e.to_str().ok())
+            == Some("TILED_SPARSE".into());
+        if is_sparse {
+            return Err("Sparsely tiled images are not supported".into());
+        }
+
+        let image_height = dcm_object
+            .element(dicom_tags::TOTAL_PIXEL_MATRIX_ROWS)?
+            .uint32()?;
+        let image_width = dcm_object
+            .element(dicom_tags::TOTAL_PIXEL_MATRIX_COLUMNS)?
+            .uint32()?;
+        let tile_height = dcm_object.element(dicom_tags::ROWS)?.uint16()?;
+        let tile_width = dcm_object.element(dicom_tags::COLUMNS)?.uint16()?;
+        let dcm_photometric_interpretation = dcm_object
+            .element(dicom_tags::PHOTOMETRIC_INTERPRETATION)?
+            .to_str()?;
+        let (tiff_photometric_interpretation, subsampling) =
+            dicom_photometric_interpretation_to_tiff(&dcm_photometric_interpretation)?;
+        let samples_per_pixel = dcm_object
+            .element(dicom_tags::SAMPLES_PER_PIXEL)?
+            .uint16()?;
+        let shared_functional_groups_sequence = match dcm_object
+            .element(dicom_tags::SHARED_FUNCTIONAL_GROUPS_SEQUENCE)?
+            .clone()
+            .into_value()
+        {
+            DicomValue::Sequence(seq) => seq,
+            _ => return Err("Expected SHARED_FUNCTIONAL_GROUPS_SEQUENCE to be a sequence".into()),
+        };
+        let shared_functional_groups_items = shared_functional_groups_sequence.into_items();
+        if shared_functional_groups_items.is_empty() {
+            return Err("SHARED_FUNCTIONAL_GROUPS_SEQUENCE is empty".into());
+        }
+        let first_shared_functional_group = &shared_functional_groups_items[0];
+        let pixel_measures_sequence = match first_shared_functional_group
+            .element(dicom_tags::PIXEL_MEASURES_SEQUENCE)?
+            .clone()
+            .into_value()
+        {
+            DicomValue::Sequence(seq) => seq,
+            _ => return Err("Expected PIXEL_MEASURES_SEQUENCE to be a sequence".into()),
+        };
+        let pixel_measures_items = pixel_measures_sequence.into_items();
+        if pixel_measures_items.is_empty() {
+            return Err("PIXEL_MEASURES_SEQUENCE is empty".into());
+        }
+        let first_pixel_measures = &pixel_measures_items[0];
+        let pixel_spacing_strs = first_pixel_measures
+            .element(dicom_tags::PIXEL_SPACING)?
+            .strings()?;
+        let pixel_spacing = pixel_spacing_strs
+            .iter()
+            .map(|s| s.trim().parse::<f64>())
+            .collect::<Result<Vec<_>, _>>()?;
+        if pixel_spacing.len() != 2 {
+            return Err("Expected PIXEL_SPACING to have 2 values".into());
+        }
+        let pixel_spacing_x = pixel_spacing[0];
+        let pixel_spacing_y = pixel_spacing[1];
+        let mpp_x = pixel_spacing_x * 1000.0;
+        let mpp_y = pixel_spacing_y * 1000.0;
+        // Centimeters
+        let x_resolution = 10000.0 / mpp_x;
+        let y_resolution = 10000.0 / mpp_y;
+
+        let bits_stored = dcm_object.element(dicom_tags::BITS_STORED)?.uint16()?;
+        let bits_per_sample = vec![bits_stored; samples_per_pixel as usize];
+
+        // TODO: What if not lossy (e.g. LZW or not compressed, etc)?
+        let lossy_compression_method = dcm_object
+            .element(dicom_tags::LOSSY_IMAGE_COMPRESSION_METHOD)?
+            .string()?
+            .trim();
+        let tiff_compression = match (lossy_compression_method, tiff_photometric_interpretation) {
+            // ("ISO_10918_1", _) => tiff::tags::CompressionMethod::JPEG,
+            ("ISO_10918_1", _) => tiff::tags::CompressionMethod::ModernJPEG,
+            // ("ISO_15444_1", _) => tiff::tags::CompressionMethod::Unknown(34712),
+            // APERIO_COMPRESSION_JP2K_RGB
+            ("ISO_15444_1", TiffPhotometricInterpretation::RGB) => {
+                tiff::tags::CompressionMethod::Unknown(33005)
+            }
+            // APERIO_COMPRESSION_JP2K_YCBCR
+            ("ISO_15444_1", TiffPhotometricInterpretation::YCbCr) => {
+                tiff::tags::CompressionMethod::Unknown(33003)
+            }
+            _ => {
+                return Err("Unsupported lossy compression method".into());
+            }
+        };
+
+        let optical_path_sequence = dcm_object
+            .element(dicom_tags::OPTICAL_PATH_SEQUENCE)?
+            .clone()
+            .into_value();
+        let optical_path_items = match optical_path_sequence {
+            DicomValue::Sequence(seq) => seq.into_items(),
+            _ => return Err("Expected OPTICAL_PATH_SEQUENCE to be a sequence".into()),
+        };
+        if optical_path_items.is_empty() {
+            return Err("OPTICAL_PATH_SEQUENCE is empty".into());
+        }
+        let icc_profile_item = optical_path_items.iter().find(|item| {
+            item.element_opt(dicom_tags::ICC_PROFILE)
+                .ok()
+                .flatten()
+                .is_some()
+        });
+        let icc_profile = icc_profile_item.map(|item| {
+            let elem = item.element(dicom_tags::ICC_PROFILE).unwrap();
+            let value = elem.clone().into_value();
+            let bytes = value.to_bytes().unwrap();
+            bytes.to_vec()
+        });
+
+        let pixel_data = dcm_object
+            .element(dicom_tags::PIXEL_DATA)?
+            .fragments()
+            .ok_or("PIXEL_DATA is of wrong type")?;
+
+        let mut dir = tiff.image_directory()?;
+
+        // Fake Aperio SVS
+        let image_description = format!("Aperio\n|MPP={}", mpp_x);
+        dir.write_tag(TiffTag::ImageDescription, image_description.as_str())?;
+
+        // Dimensions
+        dir.write_tag(TiffTag::ImageWidth, image_width)?;
+        dir.write_tag(TiffTag::ImageLength, image_height)?;
+        dir.write_tag(TiffTag::TileWidth, tile_width)?;
+        dir.write_tag(TiffTag::TileLength, tile_height)?;
+        // Resolution (MPP)
+        dir.write_tag(TiffTag::ResolutionUnit, 3)?; // 3 = centimeters
+        dir.write_tag(TiffTag::XResolution, x_resolution)?;
+        dir.write_tag(TiffTag::YResolution, y_resolution)?;
+        // Image related
+        dir.write_tag(
+            TiffTag::PhotometricInterpretation,
+            tiff_photometric_interpretation.to_u16(),
+        )?;
+        // Tag: YCbCrSubSampling
+        let ycbcr_subsampling_tag = TiffTag::Unknown(530);
+        if let Some(subsampling) = subsampling {
+            dir.write_tag(ycbcr_subsampling_tag, &subsampling[..])?;
+        }
+        dir.write_tag(TiffTag::SamplesPerPixel, samples_per_pixel)?;
+        dir.write_tag(TiffTag::BitsPerSample, &bits_per_sample[..])?;
+
+        dir.write_tag(TiffTag::Compression, tiff_compression.to_u16())?;
+
+        // TODO: If compression is JPEG, extract the JPEG tables from the pixel data
+        // (checking that they all are the same, which they should be) and set the
+        // `TiffTag::JpegTables` tag with the shared JPEG tables.
+
+        if let Some(icc_profile) = icc_profile {
+            dir.write_tag(TiffTag::IccProfile, &icc_profile[..])?;
+        }
+
+        // Image Data
+        let mut offsets = Vec::with_capacity(pixel_data.len());
+        let mut byte_counts = Vec::with_capacity(pixel_data.len());
+        for tile in pixel_data {
+            let byte_count = tile.len() as u64;
+            let offset = dir.write_data(&tile[..])?;
+            offsets.push(TiffKindBig::convert_offset(offset)?);
+            // let byte_count = dir.last_written();
+            byte_counts.push(TiffKindBig::convert_offset(byte_count)?);
+        }
+        dir.write_tag(TiffTag::TileOffsets, TiffKindBig::convert_slice(&offsets))?;
+        dir.write_tag(
+            TiffTag::TileByteCounts,
+            TiffKindBig::convert_slice(&byte_counts),
+        )?;
+
+        dir.finish()?;
+    }
+
+    Ok(())
+}
